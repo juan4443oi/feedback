9f4632b9ad07e4ec9f1447451579df9d878a3f75
Juan kaman <juankaman1202@gmail.com>
2024-06-08 20:32:15 +0000

    style: posicionamento do card no centro da tela

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,5 +1,5 @@
 export const theme = {
-    homebackground: "#131518",
+    homeBackground: "#131518",
     cardGradient: "radial-gradient(98.96% 98.96% at 50% 0%, #232a43 0%, #181e27 100%)",
     textColor: "#969fad",
     submitButtonColor: "#fc7614",
