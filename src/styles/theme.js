fc37798fd25bc123e4e3debf5b949153209604f8
Juan kaman <juankaman1202@gmail.com>
2024-06-08 17:04:35 +0000

    feat: instalação e implantação do react-router-dom e do styled-components

---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+    homebackground: "#131518",
+    cardGradient: "radial-gradient(98.96% 98.96% at 50% 0%, #232a43 0%, #181e27 100%)",
+    textColor: "#969fad",
+    submitButtonColor: "#fc7614",
+    white: "#fff",
+    feedbackButtonColor: "#262e38",
+    feedbackButtonText: "#7c8798",
+
+}
